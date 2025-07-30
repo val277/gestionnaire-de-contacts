3cb47388b48ec2fc2d0820a4b3da563d6263e94b
pierre <pierrebilotto58@gmail.com>
2025-07-30 16:35:58 +0200

    derniers ajustements css et classes

--- modules/nombre_contact.js
+++ modules/nombre_contact.js
@@ -1,8 +1,11 @@
 export function afficherMessageUtilisateur(contacts) {
-  const section = document.createElement("section"); // Créer un paragraphe <p>
+  const section = document.createElement("section");
+  section.classList.add("nombreContact");
+  // Créer un paragraphe <p>
   document.querySelector("main").appendChild(section);
 
   const message = document.createElement("p");
-  message.textContent = contacts.length; // j'ajoute le texte en prenant la longueur du tableau
+  message.textContent = contacts.length;
+  // j'ajoute le texte en prenant la longueur du tableau
   section.appendChild(message); // j’ajoute le message à la page
 }
