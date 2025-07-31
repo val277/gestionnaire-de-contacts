3f432534cc9fdbfead529f9f56110d8ed6dd30fe
Elodie663 <elodie.molieres14@gmail.com>
2025-07-31 09:22:07 +0200

    Ajout des commentaires modules nombre_contact et liste_contact

--- modules/liste_contact.js
+++ modules/liste_contact.js
@@ -1,21 +1,15 @@
-/*consignes : 
-Pour chaque objet dans notre liste tu rajoutes un élément dans le html:
-Parcourir la liste
-Créer un element avec chaque paramètre de chaque objet en textContent
-*/
-
 export function afficherListeContacts(contacts) {
-  const section = document.createElement("section"); //ici je crée ma section
-  document.querySelector("main").appendChild(section);
+  //export de la fonction réutilisée dans le main.js
+  const section = document.createElement("section"); //création de la section dans le HTML
+  document.querySelector("main").appendChild(section); //insertion de la section dans la balise <main> du document
 
-  const ul = document.createElement("ul"); //ici je crée ma liste de contacts
-  section.appendChild(ul);
-  // li.textContent = `Nom: ${contacts.nom}, Prénom: ${contacts.prenom}, Numéro: ${contacts.numero}`;
-  // ul.appendChild(li);
+  const ul = document.createElement("ul"); //création d'une liste de contacts dans le HTML
+  section.appendChild(ul); //ajout de la liste à la section créée plus haut
 
   contacts.forEach((element) => {
-    const li = document.createElement("li");
-    li.textContent = `Nom: ${element.nom}, Prénom: ${element.prenom}, Numéro: ${element.numero}`;
-    ul.appendChild(li);
+    //boucle forEach, qui va parcourir chaque élément du tableau
+    const li = document.createElement("li"); //pour chaque contact, création d'une puce <li>
+    li.textContent = `Nom: ${element.nom}, Prénom: ${element.prenom}, Numéro: ${element.numero}`; //ajout du texte en utilisant les propriétés, 'nom', 'prenom' et 'numero'
+    ul.appendChild(li); //ajout de la puce <li> à la liste <ul>
   });
 }
